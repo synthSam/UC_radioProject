d6f701547fd07326db168f7b4ee5c309a76aaf25
synthSam <112721075+synthSam@users.noreply.github.com>
2025-08-26 13:00:15 -0400

    Update newArchive.c
    new commit

--- newArchive.c
+++ newArchive.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
+#define size 10
+
+struct node{
+        int code;
+        char name[size];
+};
+
 int main(){
+        struct code elem1;
         printf("%i", true);
         return 0;
 }
