36b819fac4034d7a9269b6d3ced322b9eee580ed
synthSam <112721075+synthSam@users.noreply.github.com>
2025-08-28 14:03:35 -0400

    Update grilla_withEntryFormat.c

--- grilla_withEntryFormat.c
+++ grilla_withEntryFormat.c
@@ -10,18 +10,39 @@ typedef int timeCtrl[3];
 
 typedef struct emitionCtrl {
     char type;
+    //c,s,p
     char name[size];
-} tag;
+    timeCtrl duration;
+    //no emitions have a duration over 30 minutes
+    int popularity;
+    //in s -> [1,100] in p -> [1,10]
+    int repetitions;
+} emition;
 
 struct grid {
     timeCtrl position;
-    tag emition;
+    emition tag;
 };
 
+void set_type(emition *value){
+    if(value->type == 'c'){
+        value->repetitions = 0;
+        printf("its a song\n");
+    }
+    if(value->type == 's'){
+        value->duration[1] = 0;
+        printf("its a radio show\n");
+    }
+    if(value->type == 'p'){
+        printf("ok its an add\n");
+    }
+}
+
 int main(){
-    struct grid data[MAXDATA];
-    
-    
+    struct grid data;
+    data.tag.type = 'c';
+    set_type(&data.tag);
+    printf("%i\n", data.tag.repetitions);
     
     return 0;
 }
