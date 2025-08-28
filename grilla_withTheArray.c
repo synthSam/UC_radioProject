5b41b3e67f13a128a73df8200f0c9ec6bf52c190
synthSam <112721075+synthSam@users.noreply.github.com>
2025-08-28 12:48:40 -0400

    Update grilla_withTheArray.c
    new function for printing

--- grilla_withTheArray.c
+++ grilla_withTheArray.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#define MAXDATA 999
 #define size 30
 
 typedef int timeCtrl[3];
+//timeCtrl[] --> 0.hours 1.min 2.sec
 
 typedef struct emitionCtrl {
     char type;
@@ -15,17 +17,39 @@ struct grid {
     tag emition;
 };
 
+//funcitions
+void p_time(struct grid info);
+
 int main(){
-    struct grid data[10];
-    data[0].position[0] = 10;
-    for(int i=0; i<3; i++){
-         data[0].position[i] = (i+1) * 10;
-    }
-    strcpy(data[0].emition.name, "warhammer");
+    struct grid data[MAXDATA];
     
+    //position in
     for(int i=0; i<3; i++){
-        printf("%i:", data[0].position[i]);
+        for(int j=0; j<3; j++){
+            data[i].position[j] = (j+1)*10;
+        }
     }
-    //printf("%i:%i:%i -- %s",data[0].position[0],data[0].position[1],data[0].position[2], data[0].emition.name);
+    
+    //emition in
+    for(int i=0; i<3; i++){
+        data[i].emition.type = 'S';
+        strcpy(data[i].emition.name, "warhammer");
+    }
+    
+    //print this thing
+    for(int i=0; i<3; i++){
+        p_time(data[i]);
+        printf("%c %s\n", data[i].emition.type, data[i].emition.name);
+    }
+    
+    
+    
     return 0;
+}
+
+void p_time(struct grid info){
+    for(int i=0; i<3; i++){
+        printf("%i:", info.position[i]);
+    }
+    printf("\t");
 }
