c15e3a6d155be0c3d663a6664d426b9e28dd41c6
synthSam <112721075+synthSam@users.noreply.github.com>
2025-09-09 13:04:46 -0400

    Update newArchive.c
    first real update

--- newArchive.c
+++ newArchive.c
@@ -3,7 +3,6 @@
 
 //global constants
 #define MAX_SONGS 1000
-#define 
 //song constants
 #define SONG_NAME_MAX 50
 #define SONG_MIN 15
@@ -20,29 +19,78 @@
 #define SHOW_PREF_MIN 1
 #define SHOW_PREF_MAX 10
 
-struct song{
-        char name[];
-        int min;
-        int sec;
-        int popularity;
+#define name_size 30
+
+//structures definitions
+typedef int timeCtrl[3];
+//timeCtrl[hour, min, sec]
+
+typedef struct emitionCtrl {
+    char type;
+    //c,s,p
+    char name[name_size];
+    timeCtrl duration;
+    //no emitions have a duration over 30 minutes
+    int popularity;
+    //in c -> [1,100] in p -> [1,10]
+    int repetitions;
+} emition;
+
+struct grid {
+    timeCtrl position;
+    emition tag;
 };
 
-struct add{
-        char companie[];
-        int sec;
-        int repetitions;
-};
+//establece el tipo de emicion de la entrada
+void set_type(emition *value){
+    if(value->type == 'c'){
+        value->repetitions = 0;
+        printf("its a song\n");
+    }
+    if(value->type == 's'){
+        value->duration[1] = 0;
+        printf("its a radio show\n");
+    }
+    if(value->type == 'p'){
+        printf("ok its an add\n");
+    }
+}
 
-struct show{
-        char name[]
-        int min;
-        int sec;
-        int segments;
-        int preferencia;
-};
+//encuentra la entrada mas popular
+void thePopular(struct grid arr[], int limit){
+    int discarted[limit];
+    int discard_i = 0;
+        
+    struct grid popSon;
+    struct grid popShw;
+    popSon.tag.popularity = 0;
+    popShw.tag.popularity = 0;
+        
+    for(int i=0; i<limit; i++){
+        if(arr[i].tag.type == 'c'){
+            if(arr[i].tag.popularity > popSon.tag.popularity){
+                popSon.tag.popularity = arr[i].tag.popularity;
+                discarted[discard_i] = i;
+                discard_i++;
+            }
+        }
+            
+        if(arr[i].tag.type == 's'){
+            if(arr[i].tag.popularity > popShw.tag.popularity){
+                popShw.tag.popularity = arr[i].tag.popularity;
+                discarted[discard_i] = i;
+                discard_i++;
+            }
+        }
+    }
+        
+    printf("pop c %i\n", popSon.tag.popularity);
+    printf("pop s %i\n", popShw.tag.popularity);
+}
 
 int main() {
     printf("%i\n", true);
-    
     return 0;
 }
+
+
